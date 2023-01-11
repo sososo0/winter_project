c4f7eb7de2afbab3396b48534380d999300bff56
sososo <sh0000@pusan.ac.kr>
2023-01-11 17:40:06 +0900

    2nd commit

---
+++ login.js
@@ -0,0 +1,19 @@
+const login_ID = document.getElementById('ID');
+const login_PW = document.getElementById('PW');
+
+function getLogin() {
+    if( (login_ID.value == 'abc') && (login_PW.value == 123) ) {
+        
+        alert('Hi!');
+
+        //location.href = 'main.html';
+    }
+}
+
+function searchPW() {
+    location.href = 'main.html';
+}
+
+function getJoin() {
+    location.href = 'main.html';
+}
